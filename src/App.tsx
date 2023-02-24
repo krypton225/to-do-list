76501fba7fe495ec3eff4d3e0ad47b7dc471c431
Khaled Mohamed <k.mohamed.11.98@gmail.com>
2023-02-24 08:19:12 +0200

    ADD structure & EDIT tsconfig

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
-function App() {
-    return <div className="App">App</div>;
+import { ReactElement } from "react";
+
+function App(): ReactElement {
+    return (
+        <div className="App">
+            <div className="header">
+                <input type="text" name="task" placeholder="Add new task.." />
+                <button className="add-btn">Add task</button>
+            </div>
+
+            <div className="list">List</div>
+        </div>
+    );
 }
 
 export default App;
